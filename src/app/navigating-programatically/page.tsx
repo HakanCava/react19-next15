5ae7cac6b332c441e10408d7aa85a9c366323f06
HakanCava <hakancavaa@gmail.com>
2025-02-12 00:09:07 +0300

    feat: used useRouter

--- src/app/navigating-programatically/page.tsx
+++ src/app/navigating-programatically/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
 
 const NavProgrammatically = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/");
+  };
   return (
-    <div>NavProgrammatically</div>
-  )
-}
+    <div>
+      <h2>NavProgrammatically</h2>
+      <button className="border border-red-500 p-3 bg-red-500 rounded-md mt-3" onClick={handleClick}>go to home after some cases</button>
+    </div>
+  );
+};
 
-export default NavProgrammatically
+export default NavProgrammatically;
